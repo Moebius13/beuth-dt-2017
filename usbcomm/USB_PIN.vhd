854fced1da17e35776df67bde54a29cbdfb60604
Moebius13 <Moebius13@users.noreply.github.com>
2017-05-24 16:26:46 +0200

    add pin check button
    slight adjustment

--- usbcomm/USB_PIN.vhd
+++ usbcomm/USB_PIN.vhd
@@ -1,13 +1,16 @@
 entity USB_PIN is
 	port(SW:in bit_vector(0 to 3);
+		check:in bit;
 		pin_valid:out bit);
 end entity USB_PIN;
 
 architecture USB_PIN_a of USB_PIN is
 	constant PIN:bit_vector(0 to 3) := "1010"
-	process(SW) begin
-		with SW select
-			pin_valid <= '1' when PIN,
-				'0' when others;
+	process(check) begin
+		if(check’event and check = ’0’ and check’last_value = ’1’) then
+			with SW select
+				pin_valid <= '1' when PIN,
+					'0' when others;
+		end if;
 	end process;
 end architecture USB_PIN_a;
